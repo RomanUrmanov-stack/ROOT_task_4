3a99f103adfb835cd65562890c12ca0f529fdf0f
romanurmanov <urmanov10@gmail.com>
2020-09-23 21:23:46 -0400

    commit message

--- task_4.C
+++ task_4.C
@@ -19,8 +19,36 @@ Int_t task_4()
         if(rand_y <= our_function->Eval(rand_x)) result->Fill(rand_x);
     }
 
-    result->Draw("hist");
+    // result->Draw("hist");
     result->SetFillColor(kBlue);
+
+    //task 2
+    TGraph* rms_error = new TGraph();
+    TGraph* mean_error = new TGraph();
+    for(int j = 0; j < 100; j++)
+    {
+        N += 1000;
+        TH1D* temp = new TH1D("temp", "temp", sqrt(N), x_lower_boundary, x_upper_boundary);
+        for(int i = 0; i < N - 1; i++)
+        {   
+            Double_t rand_x = distributor->Uniform(x_lower_boundary, x_upper_boundary), rand_y = distributor->Uniform(0, M);
+            if(rand_y <= our_function->Eval(rand_x)) temp->Fill(rand_x);
+        }
+        rms_error->SetPoint(j, N, temp->GetRMSError());
+        mean_error->SetPoint(j, N, temp->GetMeanError());
+
+        delete temp;
+    }
+
+    // rms_error->Draw("AP");
+    // rms_error->SetMarkerStyle(1);
+    // rms_error->SetMarkerColor(kBlue);
+    // rms_error->SetMarkerSize(4);
+
+    mean_error->Draw("AP");
+    mean_error->SetMarkerStyle(1);
+    mean_error->SetMarkerColor(kBlue);
+    mean_error->SetMarkerSize(4);
 
     return 0; 
 }
